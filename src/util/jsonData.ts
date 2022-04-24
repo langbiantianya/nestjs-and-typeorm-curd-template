9aa6294bafd858cabb0dc2c43d1248bee2974c67
xxx <xx@xx.com>
2022-04-24 13:08:05 +0800

    添加静态方法

--- src/util/jsonData.ts
+++ src/util/jsonData.ts
@@ -7,4 +7,7 @@ export class JsonData {
   static success(data?: any): JsonData {
     return new JsonData('success', true, data);
   }
+  static error(msg?: string): JsonData {
+    return new JsonData(msg ? msg : 'erro', false);
+  }
 }
