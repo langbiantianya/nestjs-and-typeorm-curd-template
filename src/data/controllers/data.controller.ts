165f2fba23fd88b93b57fa5436cb560589dd5962
xxx <xx@xx.com>
2022-04-24 12:57:40 +0800

    添加分页返回数据

--- src/data/controllers/data.controller.ts
+++ src/data/controllers/data.controller.ts
@@ -1,4 +1,5 @@
-import { async } from 'rxjs';
+import { JsonPage } from './../../util/jsonPage';
+import { Page } from './../../util/page';
 import { DataEntity } from '../entities/data.entity';
 import { DataService } from '../services/data.service';
 import { JsonData } from '../../util/jsonData';
@@ -11,19 +12,31 @@ import {
   Put,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 
 @Controller('data')
 export class DataController {
   constructor(@Inject(DataService) private readonly dataService: DataService) {}
   @Get('/list')
-  async list(): Promise<JsonData> {
+  async list(@Body() data: DataEntity): Promise<JsonData> {
     // this.dataService.list().then((res) => {});
-    return new JsonData('', true, await this.dataService.list());
+    return new JsonData('', true, await this.dataService.list(data));
   }
   @Get('/page')
-  page(): JsonData {
-    return null;
+  async page(
+    @Query('page') page = 1,
+    @Query('size') size = 10,
+    @Body() query: DataEntity,
+  ): Promise<JsonData> {
+    return new JsonData(
+      '',
+      true,
+      new JsonPage(
+        new Page(page, size),
+        await this.dataService.page(page, size, query),
+      ),
+    );
   }
   @Post()
   async save(@Body() data: DataEntity): Promise<JsonData> {
@@ -36,11 +49,27 @@ export class DataController {
     return new JsonData('', true);
   }
   @Get(':id')
-  info(@Param('id') id: number): JsonData {
-    return this.info(id);
+  info(@Param('id') id: number): Promise<DataEntity> {
+    return this.dataService.info(id);
   }
   @Delete(':ids')
   delete(@Param('ids') ids: number[]): number[] {
     return ids;
   }
+
+  // cls<T>(value: Type<T>) {
+  //   const ctx: any = new value();
+
+  //   console.log(Object.keys(ctx));
+
+  //   const prototype = Object.getPrototypeOf(ctx);
+  //   console.log(Reflect.ownKeys(prototype));
+  //   console.log(Object.getOwnPropertyDescriptor(prototype, 'echo'));
+
+  //   console.log(Object.getOwnPropertyDescriptor(prototype, 'info'));
+  // }
 }
+
+// interface Type<T> extends Function {
+//   new (...args: any[]): T;
+// }
